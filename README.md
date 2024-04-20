e5512a6c5c479b63195c899fd757a233523b5a51
Aidan Lee <aidanjlee5@gmail.com>
2024-04-20 11:37:53 -0400

    Add beautiful soup method

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ Goal- running python script- searches all the topics that Summarai asks? for
 Add support for topics- take a topic-search google for said topic, scrape the website, then run the information to chatgpt, then move it to Supabase
 
 
-
+instructor
 1. Google package - beautiful soup package for scraping (Run it once a day ideally)
 2. OpenAI package for ChatGPT 4.0 -> Supabase Python client
--- agent.py
+++ agent.py
@@ -1,8 +1,21 @@
 from googlesearch import search
+from bs4 import BeautifulSoup
+import requests
 
-query ="site:https://www.livescience.com/"
+"""
+query ="site:https://sciencedaily.com/releases"
 
-#Science articles queries: https://www.science.org/content/article/, sciencedaily.com/releases instructor
-
+#Science articles queries: https://www.science.org/content/article/, sciencedaily.com/releases, sciencenews.org/article
+sciencearticles=[""]
 for j in search(query, num=10, stop=10, pause=2):
-    print(j)
+    sciencearticles.append(j)
+    
+print(sciencearticles)
+"""
+
+url = 'https://www.sciencedaily.com/releases/2024/04/240418165151.htm'
+page = requests.get(url)
+htmlcode = BeautifulSoup(page.text, 'html.parser')
+p_tags = htmlcode.find_all('p')
+for p in p_tags:
+    print(p.text)
